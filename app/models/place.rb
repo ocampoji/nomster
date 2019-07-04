6519d4fcdb579890e2e95e8bbaeae5211865ed44
Jorge Ocampo <ocajor1@hotmail.com>
2019-07-04 02:35:02 +0000

    Lesson 28

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,4 +1,8 @@
 class Place < ApplicationRecord
     belongs_to :user  
     validates :name, presence: true
+    validates :name, length: {minimum: 3}
+    validates :address, presence: true
+    validates :description, presence: true
+
 end
